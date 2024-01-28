3f7702b928fd0c665b713593919f83d01eadff3d
s26844-pj <s26844@pjwstk.edu.pl>
2024-01-28 11:35:41 +0100

    bobo

---
+++ src/main/java/com/example/demodemodemo/DemodemodemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodemodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodemodemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemodemodemoApplication.class, args);
+    }
+
+}
